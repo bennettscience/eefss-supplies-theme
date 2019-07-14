508d2a8024a5733043a17e4e924bfb2c0b46c2a2
bennettscience <brian.bennett2@gmail.com>
2019-07-13 23:13:11 -0400

    added comments, todos

--- functions.php
+++ functions.php
@@ -33,17 +33,25 @@ foreach ( $understrap_includes as $file ) {
 	require_once $filepath;
 }
 
+/**************************/
+/** ITEM REQUEST SCRIPTS **/
+/**************************/
+
+/** Register AJAX scripts */
 function eefss_register_request_scripts() {
-wp_register_script( 'request-handler', get_template_directory_uri() . '/js/request-handler.js' );
-wp_enqueue_script( 'request-handler', array('jquery'), '1.0.0', true );
-   $local_arr = array(
+
+	wp_register_script( 'request-handler', get_template_directory_uri() . '/js/request-handler.js' );
+	wp_enqueue_script( 'request-handler', array('jquery'), '1.0.0', true );
+
+    $local_arr = array(
         'ajaxurl'   => admin_url( 'admin-ajax.php' ),
 		'ajax_nonce'  => wp_create_nonce( 'request_item' ),
 		'user_id' => get_current_user_id(),
-    );
+	);
+	
     // Assign that data to our script as an JS object
     wp_localize_script( 'request-handler', 'ajax_object', $local_arr );
-} // end custom_register_admin_scripts
+} 
 add_action( 'wp_enqueue_scripts', 'eefss_register_request_scripts' );
 
 /** AJAX handler for requesting warehouse items */
@@ -51,6 +59,7 @@ add_action( 'wp_ajax_request_item', 'eefss_request_item_callback');
 add_action( 'wp_ajax_nopriv_request_item', 'eefss_request_item_callback' );
 function eefss_request_item_callback() {
 
+	// access the database
 	global $wpdb;
 
 	$request = $_POST['action'];
@@ -60,8 +69,6 @@ function eefss_request_item_callback() {
 	$user = get_user_by('id', intval($user_id));
 
 	$date = new DateTime();
-
-	// echo json_encode(array("success" => true, "result" => $user->display_name));
 
 	if($request === 'request_item') {
 		// get the ACF for the post
@@ -80,11 +87,15 @@ function eefss_request_item_callback() {
 	wp_die();
 }
 
+/*******************************/
+/** USER REGISTRATION SCRIPTS **/
+/*******************************/
 
 /** Custom user registration form */
 add_action('register_form', 'eefss_register_form');
 function eefss_register_form() {
 	
+	// TODO: Add all the schools here
 	$allowed = array(
 		'Beardsley',
 		'Beck',
@@ -94,6 +105,7 @@ function eefss_register_form() {
 	$building = ! empty( $_POST['building'] ) ? strval( $_POST['building'] ) : '';
 
 	?>
+
 	<p>
 		<label for="building"><?php esc_html_e( 'Building', 'crf' ) ?><br/>
 			<select
@@ -103,6 +115,7 @@ function eefss_register_form() {
 			       value="<?php echo esc_attr( $building ); ?>"
 			       class="input"
 			>
+
 			<?php 
 				foreach($allowed as $the_building) {
 					echo '<option value='. $the_building .'>' . $the_building .'</option>';
@@ -114,10 +127,11 @@ function eefss_register_form() {
 	<?php
 }
 
-/** Custom registration errors */
+/** Custom registration errors to display if needed */
 add_filter( 'registration_errors', 'eefss_registration_errors', 10, 3 );
 function eefss_registration_errors( $errors, $sanitized_user_login, $user_email ) {
 
+	// TODO: Add all schools
 	$allowed = array(
 		'Beardsley',
 		'Beck',
@@ -142,10 +156,9 @@ function eefss_registration_errors( $errors, $sanitized_user_login, $user_email 
 	return $errors;
 }
 
+/** Register the new user */
 add_action( 'user_register', 'eefss_user_register' );
 function eefss_user_register( $user_id ) {
-
-	// TODO: Set the username value
 
 	$allowed = array(
 		'Beardsley',
@@ -158,23 +171,11 @@ function eefss_user_register( $user_id ) {
 		$errors->add( 'building_error', __( '<strong>ERROR</strong>: Please enter a valid building name.', 'crf' ) );
 	}
 
-	// Validate the building submission
+	// Validate the building submission one last time
 	if ( ! empty( $_POST['building'] ) && in_array(strval( $_POST['building'] ), $allowed, true) ) {
 		update_user_meta( $user_id, 'building', strval( $_POST['building'] ) );
 	}
 
-}
-
-add_action( 'init', 'eefss_custom_requested_status' );
-function eefss_custom_requested_status() {
-	register_post_status( 'requested', array(
-		'label'                     => _x( 'Requested', 'eefss_warehouse_ad' ),
-		'public'                    => true,
-		'exclude_from_search'       => false,
-		'show_in_admin_all_list'    => true,
-		'show_in_admin_status_list' => true,
-		'label_count'               => _n_noop( 'Requested (%s)', 'Requested (%s)' ),
-	) );
 }
 
 add_action( 'show_user_profile', 'eefss_show_extra_profile_fields' );
@@ -200,10 +201,24 @@ function eefss_show_extra_profile_fields( $user ) {
 	<?php
 }
 
-/** Create the custom user roles, defining specific
- *  capabilities for each.
- */
+/**********************************/
+/** CUSTOM STATUSES, POSTS, ROLES */
+/**********************************/
 
+/** Register a custom 'Requested' post status */
+add_action( 'init', 'eefss_custom_requested_status' );
+function eefss_custom_requested_status() {
+	register_post_status( 'requested', array(
+		'label'                     => _x( 'Requested', 'eefss_warehouse_ad' ),
+		'public'                    => true,
+		'exclude_from_search'       => false,
+		'show_in_admin_all_list'    => true,
+		'show_in_admin_status_list' => true,
+		'label_count'               => _n_noop( 'Requested (%s)', 'Requested (%s)' ),
+	) );
+}
+
+/** Create the custom user roles **/
 add_action( 'init', 'eefss_add_custom_roles' );
 function eefss_add_custom_roles() {
 
@@ -224,53 +239,7 @@ function eefss_add_custom_roles() {
 	));
 }
 
-add_action('admin_menu', 'remove_admin_menu_items');
-/** Remove unnecessary menu items from the dashboard.
- *  Thanks to Tom Woodward for the snippets here:
- *  http://bionicteaching.com/minimal-wordpress/
- */
-
-function remove_admin_menu_items() {
-	if( current_user_can( 'administrator' ) ) { }
-		else {  
-		$remove_menu_items = array(
-			__('Posts'),
-			__('Media'),
-			__('Pages'),
-			__('Appearance'),
-			__('Plugins'),
-			__('Tools'),
-			__('Settings'),
-			__('Comments'),
-			__('Custom Fields')
-		);
-
-		global $menu;
-		end ($menu);
-		while (prev($menu)){
-			$item = explode(' ',$menu[key($menu)][0]);
-			if(in_array($item[0] != NULL?$item[0]:"" , $remove_menu_items)){
-				unset($menu[key($menu)]);
-			}
-		}
-	}
-}
-
-/** Hide ads from other authors */
-function posts_for_current_author($query) {
-    global $pagenow;
- 
-    if( 'edit.php' != $pagenow || !$query->is_admin )
-        return $query;
- 
-    if( !current_user_can( 'manage_options' ) ) {
-        global $user_ID;
-        $query->set('author', $user_ID );
-    }
-    return $query;
-}
-add_filter('pre_get_posts', 'posts_for_current_author');
-
+/** Register Community Ad post type **/
 add_action('init', 'eefss_register_community_ads');
 function eefss_register_community_ads() {
 	$labels = array(
@@ -311,12 +280,9 @@ function eefss_register_community_ads() {
     register_post_type('eefss_community_ad', $args);
 }
 
+/** Register Warehouse Ad post type **/
 add_action('init', 'eefss_register_warehouse_ads');
 function eefss_register_warehouse_ads() {
-/**
-     * Register a custom post types
-     * EEF Ad - posted by a moderator
-     */
 
 	$labels = array(
 		'name' => __('Warehouse Ads', 'en'),
@@ -355,8 +321,9 @@ function eefss_register_warehouse_ads() {
     register_post_type('eefss_warehouse_ad', $args);
 }
 
-add_action('init', 'eefss_add_role_caps');
-function eefss_add_role_caps() {
+/** Add role capabilities for managers **/
+add_action('init', 'eefss_add_manager_role_caps');
+function eefss_add_manager_role_caps() {
 
 		$role = get_role('eefss_manager');
 
@@ -381,6 +348,7 @@ function eefss_add_role_caps() {
 
 }
 
+/** Add role capabilities for teachers **/
 add_action('init', 'eefss_add_teacher_caps');
 function eefss_add_teacher_caps() {
 
@@ -394,11 +362,72 @@ function eefss_add_teacher_caps() {
 
 }
 
+/******************************/
+/** DASHBOARD CUSTOMIZATIONS **/
+/******************************/
+
+/** Remove unnecessary menu items from the dashboard.
+ *  Thanks to Tom Woodward for the snippets here:
+ *  http://bionicteaching.com/minimal-wordpress/
+ */
+add_action('admin_menu', 'remove_admin_menu_items');
+function remove_admin_menu_items() {
+	if( current_user_can( 'administrator' ) ) { }
+		else {  
+		$remove_menu_items = array(
+			__('Posts'),
+			__('Media'),
+			__('Pages'),
+			__('Appearance'),
+			__('Plugins'),
+			__('Tools'),
+			__('Settings'),
+			__('Comments'),
+			__('Custom Fields')
+		);
+
+		global $menu;
+		end ($menu);
+		while (prev($menu)){
+			$item = explode(' ',$menu[key($menu)][0]);
+			if(in_array($item[0] != NULL?$item[0]:"" , $remove_menu_items)){
+				unset($menu[key($menu)]);
+			}
+		}
+	}
+}
+
+/** Hide ads from other authors **/
+add_filter('pre_get_posts', 'posts_for_current_author');
+function posts_for_current_author($query) {
+    global $pagenow;
+ 
+    if( 'edit.php' != $pagenow || !$query->is_admin )
+        return $query;
+ 
+    if( !current_user_can( 'manage_options' ) ) {
+        global $user_ID;
+        $query->set('author', $user_ID );
+    }
+    return $query;
+}
+
+/** Add dashboard metaboxes for users based on role **/
+add_action( 'wp_dashboard_setup', 'eefss_dashboard_meta_boxes');
+function eefss_dashboard_meta_boxes() {
+	if(!current_user_can('eefss_teacher')) {
+		add_meta_box('eefss-ad-stats', __('Site Stats'), 'eefss_manager_dash_meta_display', 'dashboard', 'normal', 'high');
+	} else {
+		add_meta_box('user-community-ads', __('Your Ads'), 'eefss_teacher_dash_meta_display', 'dashboard', 'normal', 'high');
+	}
+}
+
+/** Define metabox for Manager role on dashboard **/
 function eefss_manager_dash_meta_display($data) {
 
 	wp_nonce_field(basename(__FILE__), "meta-box-nonce");
 
-	// Get the number of warehouse posts with requested and !completed
+	// Get the number of warehouse posts with requested and !complete
 	$warehouse_query = new WP_Query(array(
 		'post_type' => 'eefss_warehouse_ad',
 		'post_status' => 'requested',
@@ -415,7 +444,7 @@ function eefss_manager_dash_meta_display($data) {
 		),
 	));
 
-	// Display the number of pending community requests	
+	// Build a table
 	?>
 		<table>
 			<thead>
@@ -425,20 +454,28 @@ function eefss_manager_dash_meta_display($data) {
 				<th>Pending Community Posts</th>
 			</thead>
 			<tbody style="font-size:32px;">
-			<td><?php echo wp_count_posts('eefss_warehouse_ad')->publish; ?></td>
-			<td>
-				<a href="<?php echo home_url('wp-admin/edit.php?post_type=eefss_warehouse_ad&post_status=requested'); ?>"><?php echo $warehouse_query->found_posts; ?></a>
-			</td>
-			<td><?php echo wp_count_posts('eefss_community_ad')->publish; ?></td>
-			<td><a href="<?php echo home_url('wp-admin/edit.php?post_status=pending&post_type=eefss_community_ad'); ?>"><?php echo wp_count_posts('eefss_community_ad')->pending; ?></a></td>
+				<td>
+					<?php echo wp_count_posts('eefss_warehouse_ad')->publish; ?>
+				</td>
+				<td>
+					<a href="<?php echo home_url('wp-admin/edit.php?post_type=eefss_warehouse_ad&post_status=requested'); ?>"><?php echo $warehouse_query->found_posts; ?></a>
+				</td>
+				<td>
+					<?php echo wp_count_posts('eefss_community_ad')->publish; ?>
+				</td>
+				<td>
+					<a href="<?php echo home_url('wp-admin/edit.php?post_status=pending&post_type=eefss_community_ad'); ?>"><?php echo wp_count_posts('eefss_community_ad')->pending; ?></a>
+				</td>
 			</tbody>
-			</table>
+		</table>
 	<?php
 
 }
 
+/** Define metabox for Teacher role **/
 function eefss_teacher_dash_meta_display($data) {
 
+	// Get all posts by this author
 	$query = new WP_Query(array(
 		'post_type' => 'eefss_community_ad',
 		'post_status' => 'publish',
@@ -453,13 +490,21 @@ function eefss_teacher_dash_meta_display($data) {
 		<thead>
 			<th>Title</th>
 			<th>Posted On</th>
+			<th>Views</th>
 		</thead>
 		<tbody>
 
 		<?php while ( $query->have_posts() ) : $query->the_post(); ?>
 			<tr>
-				<td><?php the_title(); ?></td>
-				<td><?php the_time( get_option( date_format ) ); ?></td>
+				<td>
+					<?php the_title(); ?>
+				</td>
+				<td>
+					<?php the_time( get_option( date_format ) ); ?>
+				</td>
+				<td>
+					<?php echo eefss_get_post_view(); ?>
+				</td>
 			</tr>
 		
 		<?php endwhile; ?>
@@ -471,25 +516,17 @@ function eefss_teacher_dash_meta_display($data) {
 	wp_reset_query();
 }
 
-/** Add dashboard metaboxes for users */
-function eefss_dashboard_meta_boxes() {
-	if(!current_user_can('eefss_teacher')) {
-		add_meta_box('eefss-ad-stats', __('Site Stats'), 'eefss_manager_dash_meta_display', 'dashboard', 'side', 'high');
-	} else {
-		add_meta_box('user-community-ads', __('Your Ads'), 'eefss_teacher_dash_meta_display', 'dashboard','normal');
-	}
-}
-add_action( 'wp_dashboard_setup', 'eefss_dashboard_meta_boxes');
-
-
 add_filter( 'manage_posts_columns', 'eefss_posts_column_views' );
 add_action( 'manage_posts_custom_column', 'eefss_posts_custom_column_views' );
 add_action( 'wp_dashboard_setup', 'eefss_get_post_view');
 
+/** Return the number of views a post has **/
 function eefss_get_post_view() {
     $count = get_post_meta( get_the_ID(), 'post_views_count', true );
     return $count;
 }
+
+/** Store a view of a post **/
 function eefss_set_post_view() {
     $key = 'post_views_count';
     $post_id = get_the_ID();
@@ -497,11 +534,15 @@ function eefss_set_post_view() {
     $count++;
     update_post_meta( $post_id, $key, $count );
 }
+
+/** Set a 'Views' column in the admin post list **/
 function eefss_posts_column_views( $columns ) {
 	$columns['post_views'] = 'Views';
 	$columns['requested_by'] = 'Requested By';
     return $columns;
 }
+
+/** Populate custom columns **/
 function eefss_posts_custom_column_views( $column ) {
     if ( $column === 'post_views') {
         echo eefss_get_post_view();
@@ -512,31 +553,4 @@ function eefss_posts_custom_column_views( $column ) {
 
 		echo $user_email;
 	}
-}
-
-add_action('quick_edit_custom_box',  'eefss_custom_quick_edit', 10, 2);
- 
-function eefss_custom_quick_edit($column_name, $post_type) {
-    if ($column_name != 'widget_set') return;
-    ?>
-    <fieldset class="inline-edit-col-left">
-    <div class="inline-edit-col">
-        <span class="title">Widget Set</span>
-        <input type="hidden" name="shiba_widget_set_noncename" id="shiba_widget_set_noncename" value="" />
-        <?php // Get all widget sets
-            $widget_sets = get_posts( array( 'post_type' => 'widget_set',
-                            'numberposts' => -1,
-                            'post_status' => 'publish') );
-        ?>
-        <select name='post_widget_set' id='post_widget_set'>
-            <option class='widget-option' value='0'>None</option>
-            <?php 
-            foreach ($widget_sets as $widget_set) {
-                echo "<option class='widget-option' value='{$widget_set->ID}'>{$widget_set->post_title}</option>\n";
-            }
-                ?>
-        </select>
-    </div>
-    </fieldset>
-    <?php
 }
