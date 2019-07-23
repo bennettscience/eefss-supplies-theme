11b3d78bce1528fde87efdc81cd7973a86aa8cd9
bennettscience <brian.bennett2@gmail.com>
2019-07-23 14:26:07 -0400

    remove pagination from home

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			</main><!-- #main -->
 
 			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
+			<!-- <?php understrap_pagination(); ?> -->
 
 			<!-- Do the right sidebar check -->
 			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
